17e82bdd349197f8b72cafea084745a1b3820144
Alex Mykyta <alexander.mykyta@spacex.com>
2016-02-05 16:04:21 -0800

    generic app base

--- software/firmware_loader.py
+++ software/firmware_loader.py
@@ -4,45 +4,28 @@ import sys
 import argparse
 import logging
 import py_modules.skylight as skylight
+from py_modules.app import App
 
-logging.basicConfig(
-    format="%(levelname)s:%(name)s - %(message)s",
-    level=logging.INFO
-)
-log = logging.getLogger()
-
-class App:
+class FirmwareLoader(App):
+  def set_cmdline_args(self, parser):
+    App.set_cmdline_args(self, parser)
     
-    def __init__(self):
-        self.options = None
-    
-    def main(self):
-        self.get_args()
-        
-        with skylight.Skylight(self.options.port) as S:
-            S.enter_bootloader()
-            S.send_ihex(self.options.filename)
-            S.exit_bootloader()
-            S.set_time()
-        
-    def get_args(self):
-        p = argparse.ArgumentParser(description="Skylight LED Firmware Loader")
-        p.add_argument("-p --port", dest="port", default="/dev/rfcomm0",
+    parser.description = "Skylight LED Firmware Loader"
+    parser.add_argument("-p --port", dest="port", default="/dev/rfcomm0",
                         help="Serial Port Device")
-        p.add_argument("-q --quiet", dest="quiet", action="store_true", default=False,
-                        help="Disable info messages")
-        p.add_argument("--verbose", dest="verbose", action="store_true", default=False,
-                        help="Enable debug messages")
-        p.add_argument("filename",
+    parser.add_argument("filename",
                         help="Source Intel-Hex file")
-        self.options = p.parse_args()
-        
-        if(self.options.quiet):
-            log.setLevel(logging.WARNING)
-        elif(self.options.verbose):
-            log.setLevel(logging.DEBUG)
+  
+  def main(self):
+    App.main(self)
+    
+    with skylight.Skylight(self.options.port) as S:
+      S.enter_bootloader()
+      S.send_ihex(self.options.filename)
+      S.exit_bootloader()
+      S.set_time()
 
 ####################################################################################################
 if __name__ == '__main__':
-    A = App()
-    A.main()
+  A = FirmwareLoader()
+  A.main()
